2b740823acb5960bafacd743bffa392f5167c61e
Rene Tellez Rodriguez <rne1223@gmail.com>
2013-05-09 13:20:18 -0700

    Removed some of the templates that were unnecesary

--- php-version/header.php
+++ php-version/header.php
@@ -78,12 +78,12 @@
 						<li><a data-value="classic" href="#"><i class="icon-blank"></i> Classic</a></li>
 						<li><a data-value="cerulean" href="#"><i class="icon-blank"></i> Cerulean</a></li>
 						<li><a data-value="cyborg" href="#"><i class="icon-blank"></i> Cyborg</a></li>
-						<li><a data-value="redy" href="#"><i class="icon-blank"></i> Redy</a></li>
-						<li><a data-value="journal" href="#"><i class="icon-blank"></i> Journal</a></li>
-						<li><a data-value="simplex" href="#"><i class="icon-blank"></i> Simplex</a></li>
+						<!-- <li><a data-value="redy" href="#"><i class="icon-blank"></i> Redy</a></li> -->
+						<!-- <li><a data-value="journal" href="#"><i class="icon-blank"></i> Journal</a></li> -->
+						<!-- <li><a data-value="simplex" href="#"><i class="icon-blank"></i> Simplex</a></li> -->
 						<li><a data-value="slate" href="#"><i class="icon-blank"></i> Slate</a></li>
-						<li><a data-value="spacelab" href="#"><i class="icon-blank"></i> Spacelab</a></li>
-						<li><a data-value="united" href="#"><i class="icon-blank"></i> United</a></li>
+						<!-- <li><a data-value="spacelab" href="#"><i class="icon-blank"></i> Spacelab</a></li> -->
+						<!-- <li><a data-value="united" href="#"><i class="icon-blank"></i> United</a></li> -->
 					</ul>
 				</div>
 				<!-- theme selector ends -->
